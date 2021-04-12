9f151284a0f8c3c3d143249ed8f77bc93feb149d
Dhrupad Bhardwaj <db4045@nyu.edu>
2021-04-11 21:56:39 -0400

    More scripts

--- slurm_scripts/supsup_basis_normalpha_test_slurm.s
+++ slurm_scripts/supsup_basis_normalpha_test_slurm.s
@@ -4,12 +4,12 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --time=23:00:00
 #SBATCH --mem=10GB
-#SBATCH --job-name=supsup-regalpha
+#SBATCH --job-name=supsup-normalpha
 #SBATCH --mail-type=END
 #SBATCH --mail-user=db4045@nyu.edu
 #SBATCH --account=cds
 #SBATCH --gres=gpu:1
-#SBATCH --output=logs/slurm_supsup_regalpha_10_%j.out
+#SBATCH --output=logs/slurm_supsup_normalpha_10_%j.out
 
 # Remove all unused system modules
 module purge
@@ -22,17 +22,26 @@ OVERLAY=$MYPY_ROOT/containers/$MYPY_ENV.ext3
 # Move into the directory that contains our code
 SRCDIR=$(pwd)
 
+#LR=0.02
+#LR=0.005
+#WD=0.01
+#WD=0.01
+#AL=0.005
+#AN=0.75
+#AN=0.95
 LR=$1
 WD=$2
 AL=$3
 AN=$4
+
 echo "Learning Rate: "$LR
 echo "Weight Decay (L2 Adam): "$WD
 echo "Alpha Lambda: "$AL
 echo "Alpha Norm (L "$AN")"
 
 echo "Standard Basis (Hard Masks). Default"
-/home/db4045/.mypy/bin/python $SRCDIR/basis.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_runs/SupsupSeed/rn18-supsup/id=supsup~seed=0~sparsity=75~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup-basis-multitask.yaml --name 'dhrupad_supsup_regularized_alphas_hard_masks~al='$AL'~wd='$WD'~LR='$LR'~norm='$AN --log-dir=/scratch/db4045/runs/dhrupad_runs/AlphaMultiNormRandInit/ --num-seed-tasks-learned 10 --epochs 20 --al $AL --trainer alphareg --wd $WD --lr $LR --multigpu="0" --log-alphas --train_mask_alphas --alpha-norm $AN
+#/home/db4045/.mypy/bin/python $SRCDIR/basis.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_runs/SupsupSeed/rn18-supsup/id=supsup~seed=0~sparsity=75~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup-basis-multitask-normalpha.yaml --name 'dhrupad_supsup_regularized_alphas_hard_masks~al='$AL'~wd='$WD'~LR='$LR'~norm='$AN --log-dir=/scratch/db4045/runs/dhrupad_runs/NormAlphaRandInit/ --num-seed-tasks-learned 10 --epochs 15 --al $AL --trainer alphareg --wd $WD --lr $LR --multigpu="0" --log-alphas --train_mask_alphas --alpha-norm $AN
+/home/db4045/.mypy/bin/python $SRCDIR/basis.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_runs/SupsupSeed/rn18-supsup/id=supsup~seed=0~sparsity=75~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup-basis-multitask-normalpha.yaml --name 'dhrupad_supsup_regularized_alphas_hard_masks~al='$AL'~wd='$WD'~LR='$LR'~norm='$AN --log-dir=/scratch/db4045/runs/dhrupad_runs/NormAlphaRandInit/ --num-seed-tasks-learned 10 --epochs 300 --al $AL --trainer alphareg --wd $WD --lr $LR --multigpu="0" --train_mask_alphas --alpha-norm $AN --log-alphas
 
 #echo
 #echo "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"
